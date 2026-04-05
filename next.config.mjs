3ca0f3524cbee18880ba495bb9da39fed476141c
IamAgbaCoder <demiladebamgboye@gmail.com>
2026-04-05 23:52:10 +0100

    fix build issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    allowedDevOrigins: ["localhost:3000", "127.0.0.1:3000", "gitsage-api.up.railway.app"]
-  }
 };
 
 export default nextConfig;
